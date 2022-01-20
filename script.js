75b5d0a185c1e414e3b4f299a5aae3ecd401ae28
Kylie West <kmichellewest@gmail.com>
2022-01-20 13:49:31 -0700

    Add game functionality

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ const loss = "LOSS";
 const tie = "TIE";
 
 function determineResult(a, b) {
-	a === b
-		? tie
-		: a === rock && b === paper
-		? win
-		: a === rock && b === scissors
-		? loss
-		: a === paper && b === rock
-		? win
-		: a === paper && b === scissors
-		? loss
-		: a === scissors && b === paper
-		? win
-		: a === scissors && b === rock
-		? win
-		: console.log("Unable to determine result.");
+	switch (true) {
+		case a === b:
+			return tie;
+		case a === rock && b === scissors:
+			return win;
+		case a === rock && b === paper:
+			return loss;
+		case a === paper && b === rock:
+			return win;
+		case a === paper && b === scissors:
+			return loss;
+		case a === scissors && b === paper:
+			return win;
+		case a === scissors && b === rock:
+			return loss;
+		default:
+			break;
+	}
 }
 
 function computerPlay() {
 	// Randomly selects ROCK, PAPER, or SCISSORS
 	let randomNum = Math.random();
 
-	switch (randomNum) {
+	switch (true) {
 		case randomNum < 1 / 3:
 			return rock;
 		case randomNum > 1 / 3 && randomNum < 2 / 3:
@@ -39,8 +42,70 @@ function computerPlay() {
 	}
 }
 
-function playRound(playerSelection, computerSelection) {
-	playerSelection.toUpperCase();
+function playRound() {
+	let computerSelection = computerPlay();
+
+	let playerSelection = prompt("Rock, paper, or scissors?");
+	playerSelection = playerSelection.toUpperCase();
+
+	console.log(`Your selection: ${playerSelection}`);
+
+	const tieMessage = `%cThe computer also chose ${playerSelection}. It's a tie. :|`;
+	const winMessage = `%cThe computer chose ${computerSelection}! You win! :)`;
+	const lossMessage = `%cThe computer chose ${computerSelection}. You lose... :(`;
+
+	let result = determineResult(playerSelection, computerSelection);
+
+	if (result === win) {
+		console.log(winMessage, "color: green");
+	} else if (result === loss) {
+		console.log(lossMessage, "color: red");
+	} else if (result === tie) {
+		console.log(tieMessage, "color: yellow");
+	}
+
+	return result;
 }
 
-function game() {}
+function game() {
+	let wins = 0;
+	let losses = 0;
+	let ties = 0;
+
+	for (i = 1; i <= 5; i++) {
+		console.log(`Round ${i}, go!`);
+
+		let result = playRound();
+
+		if (result === win) {
+			wins++;
+		} else if (result === loss) {
+			losses++;
+		} else if (result === tie) {
+			ties++;
+		} else {
+			console.error("The result for this round is invalid.");
+		}
+
+		console.log(`Wins: ${wins} Losses: ${losses} Ties: ${ties}`);
+	}
+
+	console.log("And the winner is...");
+
+	// Create suspense
+	for (i = 0; i < 3; i++) {
+		setTimeout(() => {
+			console.log("..."), 1000;
+		});
+	}
+
+	if (wins > losses) {
+		console.log("you!!! :D");
+	} else if (wins < losses) {
+		console.log("the computer!!! Better luck next time...");
+	} else {
+		console.log("nobody. Lame...");
+	}
+}
+
+game();
